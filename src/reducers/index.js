77fc11045de0d64b58a6ca78782c2d44f089865a
kenshi sasayama <sucoshi@gmail.com>
2018-11-29 15:57:50 +0900

    implement events index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 //全reducerを結合する
 import { combineReducers } from 'redux'
-import count from './count'
+import events from './events'
 
-export default combineReducers({ count })
+export default combineReducers({ events })
 // export default combineReducers({ foo, bar, baz })
